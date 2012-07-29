302acf4e8fbf0b7c8057e059668da30ef6588235
Eric Chan <er_cha@cs.concordia.ca>
2012-07-28 21:10:29 -0400

    pulled in all of the project from local storage

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 c376balloon2d
 =============
 
-COMP376 Balloon Assignment in 2D
+COMP376 Balloon Assignment in 2D for XNA
+
+## COMP376/2 Assignment 2
+
+Eric Chan
+
+The game compiles from the c367a2 solution and can be run from visual studio.
+
+Controls:
+
+Menu:
+Up		Go up on option
+Down	Go down one option
+Enter	Select option
+Esc		Quit Game
+
+Story screen:
+Enter	Start gameplay
+
+Gameplay:
+Up/Down/Left/Right
+		Move around on the screen
+Space	Fire in last direction travelled
+Z		Fire special attack (all direction, only once for Alternate gameplay)
+Esc		Return to menu
---
+++ c376a2/c376a2/MenuScreen.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace c376a2
+{
+    class MenuScreen : ThinkDraw
+    {
+        public static SpriteFont font;
+        bool lastEscState = true;
+        bool lastUpState = true;
+        bool lastDownState = true;
+        int menuIndex = 0;
+
+        public MenuScreen()
+        {
+
+        }
+        public void think(GameTime gt)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) && !lastEscState)
+            {
+                Switcher.game.Exit();
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && !lastUpState)
+            {
+                menuIndex = Math.Max(menuIndex - 1, 0);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) && !lastDownState)
+            {
+                menuIndex = Math.Min(menuIndex + 1, 2);
+            }
+
+            lastEscState = Keyboard.GetState().IsKeyDown(Keys.Escape);
+            lastUpState = Keyboard.GetState().IsKeyDown(Keys.Up);
+            lastDownState = Keyboard.GetState().IsKeyDown(Keys.Down);
+        }
+        public void draw(SpriteBatch sb)
+        {
+            string content;
+            Color color;
+            Vector2 size;
+
+            sb.Begin();
+            sb.Draw(NormalVariant.skygrad, new Rectangle(0, 0, (int)(Switcher.size.X), (int)(Switcher.size.Y)), null, Color.White);
+
+            content = "Normal Mode";
+            color = (menuIndex == 0) ? Color.Red : Color.White;
+            size = font.MeasureString(content);
+            sb.DrawString(font, content, (Switcher.size - size) / 2 - new Vector2(0, Switcher.size.Y / 6), color);
+
+            content = "Alternate Mode";
+            color = (menuIndex == 1) ? Color.Red : Color.White;
+            size = font.MeasureString(content);
+            sb.DrawString(font, content, (Switcher.size - size) / 2, color);
+
+            content = "LOL Mode";
+            color = (menuIndex == 2) ? Color.Red : Color.White;
+            size = font.MeasureString(content);
+            sb.DrawString(font, content, (Switcher.size - size) / 2 + new Vector2(0, Switcher.size.Y / 6), color);
+
+
+            sb.End();
+        }
+        public ThinkDraw next()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                ThinkDraw g;
+                switch (menuIndex)
+                {
+                    case 0:
+                        g = new NormalVariant();
+                        ((NormalVariant)g).init(Switcher.game, Switcher.size);
+                        break;
+                    case 1:
+                        g = new StoryScreen();
+                        break;
+                    case 2:
+                        g = new DebugVariant();
+                        ((NormalVariant)g).init(Switcher.game, Switcher.size);
+                        break;
+                    default:
+                        g = new NormalVariant();
+                        ((NormalVariant)g).init(Switcher.game, Switcher.size);
+                        break;
+                }
+                
+                return g;
+            }
+
+            return null;
+        }
+    }
+}
+
